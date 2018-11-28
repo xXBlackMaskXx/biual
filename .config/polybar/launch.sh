9ee5d0d9794bdbecb02fd2125db0f75f821de56e
Dawid Potocki <dpot@tuta.io>
2018-11-28 17:21:49 +1300

    Polybar works with any wireless interface out of the box

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -9,7 +9,7 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 # Launch polybar
 if type "xrandr" > /dev/null; then
 	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-	MONITOR=$m polybar --reload topbar -c ~/.config/polybar/config &
+	WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload topbar -c ~/.config/polybar/config &	
 done
 else
 	polybar --reload topbar -c ~/.config/polybar/config &
