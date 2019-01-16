4870cc131b55769bea64815f09a7a69ddde40827
Dawid Potocki <dpot@tuta.io>
2019-01-16 23:47:12 +1300

    Formatting

--- .script/dmenu-screenshot.sh
+++ .script/dmenu-screenshot.sh
@@ -3,9 +3,9 @@
 # dmenu prompts
 TYPE="$(printf "Screen\nWindow/Area\n" | dmenu -i -p 'What do you want to screenshot?')"
 if [ -z "$TYPE" ]; then exit; fi
-SAVE="$(printf "No\nYes"        | dmenu -i -p 'Do you want to copy to clipboard?')"
+SAVE="$(printf "No\nYes"               | dmenu -i -p 'Do you want to copy to clipboard?')"
 if [ -z "$SAVE" ]; then exit; fi
-TIME="$(printf "0.7\n5\n10\n15" | dmenu -i -p 'How many seconds you want to wait?')"
+TIME="$(printf "0.7\n5\n10\n15"        | dmenu -i -p 'How many seconds you want to wait?')"
 if [ -z "$TIME" ]; then exit; fi
 
 # Set flag if selected "Window"
