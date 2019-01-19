47be41f1dd6aab88a3fdf1426695a83daf0169aa
Dawid Potocki <dpot@tuta.io>
2019-01-19 17:04:54 +1300

    Use --zoom flag instead of --maximize for setting wallpaper

--- .script/wallpaper.sh
+++ .script/wallpaper.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 # Sets the background. If given an argument, will set file as background.
 [ ! -z "$1" ] && cp "$1" ~/.config/wallpaper && notify-send -i "$HOME/.config/wallpaper" "Wallpaper changed."
-xwallpaper --daemon --maximize ~/.config/wallpaper
+xwallpaper --daemon --zoom ~/.config/wallpaper
