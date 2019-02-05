90cde26d732680c4c06ff0bbe00e3c13d0fe921c
Dawid Potocki <dpot@disroot.org>
2019-02-06 02:43:45 +1300

    Change init 6 to 0 so it will now shutdown and not reboot

--- .script/dmenu-logout.sh
+++ .script/dmenu-logout.sh
@@ -3,7 +3,7 @@
 ANS="$(printf "Lock\nShutdown\nReboot\nExit" | dmenu -i -p 'System')"
 case "$ANS" in
 	Lock) i3lock -c 000000;;
-	Shutdown) sudo init 6;;
+	Shutdown) sudo init 0;;
 	Reboot) sudo reboot;;
 	Exit) i3-msg exit
 esac
