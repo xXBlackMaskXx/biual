7dbf8c53b67f25dcd431c6e6b0cd043ed4a7ac35
Dawid Potocki <dpot@disroot.org>
2019-06-03 06:14:55 +1200

    Add wired connection indicator

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,5 +8,5 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch polybar
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-	WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload topbar &
+	WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') WIRED=$(ls /sys/class/net/ | grep ^en | awk 'NR==1{print $1}') MONITOR=$m polybar --reload topbar &
 done
