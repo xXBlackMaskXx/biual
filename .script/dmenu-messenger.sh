2b8b1b722b4b3c4c9bbb888ae7647cd48118eae1
Dawid Potocki <dpot@disroot.org>
2019-06-07 00:21:43 +1200

    Add Wire to messenger list and not even try to route Signal through Tor,
    because it doesn't even work.

--- .script/dmenu-messenger.sh
+++ .script/dmenu-messenger.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env sh
 
-ANS="$(printf "Riot (Tor :9050)\nRiot (Tor :9150)\nRiot\nTelegram\nSignal" | dmenu -i -p 'Messenger')"
+ANS="$(printf "Riot (Tor :9050)\nRiot (Tor :9150)\nRiot\nTelegram\nSignal\nWire (Tor :9050)\nWire (Tor :9150)\nWire" | dmenu -i -p 'Messenger')"
 case "$ANS" in
 	"Riot (Tor :9050)") riot-desktop --proxy-server=socks5://127.0.0.1:9050;;
 	"Riot (Tor :9150)") riot-desktop --proxy-server=socks5://127.0.0.1:9150;;
 	"Riot") riot-desktop;;
 	"Telegram") Telegram;;
-	"Signal") signal-desktop --proxy-server=socks5://127.0.0.1:9050
+	"Signal") signal-desktop;;
+	"Wire (Tor :9050)") wire-desktop --proxy-server=socks5://127.0.0.1:9050;;
+	"Wire (Tor :9150)") wire-desktop --proxy-server=socks5://127.0.0.1:9150;;
+	"Wire") wire-desktop
 esac
