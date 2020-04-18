ae975ce5c506e7f62559092465f037a178c07bfc
Dawid Potocki <dawid@dawidpotocki.com>
2020-04-18 23:46:26 +1200

    Change `#!/usr/bin/env sh` to `#!/bin/sh`

--- .script/dmenu-logout.sh
+++ .script/dmenu-logout.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/bin/sh
 
 ANS="$(printf "Lock\nShutdown\nReboot\nExit" | dmenu -i -p 'System')"
 case "$ANS" in
--- .script/wallpaper.sh
+++ .script/wallpaper.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/bin/sh
 # Sets the background. If given an argument, will set file as background.
 [ -n "$1" ] && cp "$1" ~/.config/wallpaper && notify-send -i "$HOME/.config/wallpaper" "Wallpaper changed."
 xwallpaper --daemon --zoom ~/.config/wallpaper
