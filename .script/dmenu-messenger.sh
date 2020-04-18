d9a53ed4f5a48392df40867c79d16cbace18e5ea
Dawid Potocki <dawid@dawidpotocki.com>
2020-04-18 23:09:33 +1200

    Add option to add profiles to messengers in ~/.script/dmenu-messenger.sh

--- .script/dmenu-messenger.sh
+++ .script/dmenu-messenger.sh
@@ -1,12 +1,26 @@
-#!/usr/bin/env sh
+#!/bin/sh
 
-ANS="$(printf "Riot (Tor :9050)\nRiot (Tor :9150)\nRiot\nSignal\nWire (Tor :9050)\nWire (Tor :9150)\nWire" | dmenu -i -p 'Messenger')"
-case "$ANS" in
-	"Riot (Tor :9050)") riot-desktop --proxy-server=socks5://127.0.0.1:9050;;
-	"Riot (Tor :9150)") riot-desktop --proxy-server=socks5://127.0.0.1:9150;;
-	"Riot") riot-desktop;;
-	"Signal") signal-desktop;;
-	"Wire (Tor :9050)") wire-desktop --proxy-server=socks5://127.0.0.1:9050;;
-	"Wire (Tor :9150)") wire-desktop --proxy-server=socks5://127.0.0.1:9150;;
-	"Wire") wire-desktop
+messenger="$(printf "Riot (Tor :9050)\nRiot (Tor :9150)\nRiot\nKeybase\nSignal" | dmenu -i -p 'Messenger')"
+case "$messenger" in
+	"Riot (Tor :9050)") exe="riot-desktop --proxy-server=socks5://127.0.0.1:9050" app="Riot";;
+	"Riot (Tor :9150)") exe="riot-desktop --proxy-server=socks5://127.0.0.1:9150" app="Riot";;
+	"Riot") exe="riot-desktop" app="Riot";;
+	"Keybase") exe="run_keybase" app="Keybase";;
+	"Signal") exe="signal-desktop" app="Signal";;
 esac
+
+
+# ~/.config/messenger-profiles template:
+# app_name:profile_name
+
+if [ -n "$exe" ]; then
+	if grep "$app" ~/.config/messenger-profiles; then
+		profile="$(printf "Default\n%s" "$(grep "$app" ~/.config/messenger-profiles | cut -d: -f2)" | dmenu -i -p 'Profile')"
+		case "$profile" in
+			"Default") $exe;;
+			*) $exe --profile "$(printf "%s" "$profile" | cut -d: -f2)";;
+		esac
+	else
+		$exe
+	fi
+fi
